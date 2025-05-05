3cdc61968d55e513efb099d8c0843c1666f2d3e8
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-06 03:26:28 +0530

    commit url module

---
+++ BuiltInModules/url.module.js
@@ -0,0 +1,61 @@
+const url = require('url');
+
+//URL Object conversion...........
+//url.parse();
+
+const myUrl = url.parse('https://www.example.com:3000/pathname?search=test#hash');
+
+//console.log(myUrl);
+
+
+
+//URLObject conversion URL String...........
+//url.format();
+
+
+const urlObject = {
+    protocol: 'https://',
+    hostname: 'www.example.com',
+    pathname: '/pathname',
+    search: '?search=test'
+  };
+  
+  const formattedUrl = url.format(urlObject);
+  
+  //console.log(formattedUrl); 
+
+
+
+  //Two url add....
+//url.resolve();
+
+
+
+
+  const base = 'https://www.example.com/path/';
+const relative = 'anotherPath';
+
+
+const resolvedUrl = url.resolve(base, relative);
+
+//console.log(resolvedUrl); 
+
+
+
+//URL DETAIL..............
+
+const myUrl1 = new URL('https://www.example.com/pathname?search=test#hash');
+
+// console.log(myUrl1.hostname);  
+// console.log(myUrl1.pathname);  
+// console.log(myUrl1.search);    
+
+
+//url format()........................
+
+
+const myUrl2 = new URL('https://www.example.com/pathname?search=test');
+
+myUrl2.searchParams.append('newParam', 'value');  
+
+//console.log(myUrl2.href); 
