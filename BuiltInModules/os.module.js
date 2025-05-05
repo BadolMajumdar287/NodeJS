5e4afcbe844e4afe2c09482668b6f57b0169eee4
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-06 03:05:52 +0530

    commit os module

---
+++ BuiltInModules/os.module.js
@@ -0,0 +1,80 @@
+ const os = require('os');
+
+
+ //system platfrom information...
+ //os.platform();
+
+
+
+ const system_Platfrom_information = os.platform();
+
+ //console.log(system_Platfrom_information);
+
+
+ //CPU information.....
+ //os.cpus();
+
+
+
+ const Cpu_information = os.cpus();
+
+ //console.log(Cpu_information);
+
+
+ //free RAM...
+ //os.freemem();
+
+
+
+ const Free_RAM = os.freemem();
+ //console.log(Free_RAM);
+
+
+ //Total memory.......
+ //os.totalmem();
+
+
+ const total_memory = os.totalmem();
+ //console.log(total_memory);
+
+
+
+ //Home dir path.....
+ //os.homedir();
+
+ const home_dir = os.homedir();
+ //console.log(home_dir);
+
+
+ //System uptime..........
+//os.uptime();
+
+
+
+const up_time = os.uptime();
+//console.log(up_time);
+
+
+
+//CPU Architecture...........
+//os.arch();
+
+const Cpu_ar = os.arch();
+
+//console.log(Cpu_ar);
+
+
+//user information.........
+//os.userInfo();
+
+const user_Information = os.userInfo();
+//console.log(user_Information);
+
+
+//Relage os.................
+//os.release();
+
+
+const release_os = os.release();
+
+//console.log(release_os);
