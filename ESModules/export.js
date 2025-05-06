32896b8f3c25261384306f4386ac16c923b5b6a4
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-06 16:05:27 +0530

    commit ESModule

---
+++ ESModules/export.js
@@ -0,0 +1,23 @@
+
+//package.json file a
+//{
+ // "type": "module"
+//}
+
+
+
+export function add(a,b){
+    return a+b;
+};
+
+export function sub(a,b){
+    return a-b;
+};
+
+export function mul(a,b){
+    return a*b;
+};
+
+export function divi(a,b){
+    return a/b;
+}
---
+++ ESModules/import.js
@@ -0,0 +1,9 @@
+import { add,sub,mul,divi } from "./export.js";
+
+
+const addResult = add(2,2);
+const subResult = sub(2,2);
+const mulResult = mul(2,2);
+const diviResult = divi(2,2);
+
+console.log(addResult,subResult,mulResult,diviResult)
