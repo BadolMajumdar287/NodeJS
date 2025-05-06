d119243170993576254ca14458297c934bdca269
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-06 16:23:49 +0530

    commit CommonJs Module

---
+++ CommonJSModuleSystem/import.js
@@ -0,0 +1,11 @@
+const math = require("./math.js");
+
+
+
+
+const addResult = math.add(2,2);
+const subResult = math.sub(2,2);
+const mulResult = math.mul(2,2);
+const diviResult = math.divi(2,2);
+
+console.log(addResult,subResult,mulResult,diviResult)
---
+++ CommonJSModuleSystem/math.js
@@ -0,0 +1,27 @@
+
+function add(a,b){
+    return a+b;
+};
+
+ function sub(a,b){
+    return a-b;
+};
+
+ function mul(a,b){
+    return a*b;
+};
+
+ function divi(a,b){
+    return a/b;
+};
+
+
+
+module.exports = {
+
+  add,
+  sub,
+  mul,
+  divi
+
+}
